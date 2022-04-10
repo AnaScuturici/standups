7448a584306853a8aa2f0c39a3da75f32e9f1c37
ana scuturici <anascuturici@gmail.com>
2022-04-10 21:05:33 +0900

    add: goals path in index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Goals from './components/Goals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
           <Route path="/" element={<App />}/>
+          <Route path="/goals" element={<Goals />}/>
       </Routes>
     </Router>
   </React.StrictMode>,
