6e40c0670de9a79d23b786c1fd61b577f28a76e1
ana scuturici <anascuturici@gmail.com>
2022-04-10 14:58:36 +0900

    add: fetch api data with axios

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import Goals from './components/Goals';
+import axios from "axios";
+
+const API_URL = "http://[::1]:3000/api/v1/standups";
+
+const getAPIData = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+}
 
 function App() {
 
