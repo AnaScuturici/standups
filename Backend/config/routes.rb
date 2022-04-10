449fe1b1e03f5519e0b8f34fdcbdee7f18a0f70a
ana scuturici <anascuturici@gmail.com>
2022-04-10 14:10:07 +0900

    add: define routes

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :standups
+  namespace :api do
+    namespace :v1 do
+      resources :standups
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
