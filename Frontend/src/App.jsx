1464d3d3eee8d27862ab6a07c307e04ae6400e47
ana scuturici <anascuturici@gmail.com>
2022-04-11 00:05:06 +0900

    add: addGoal in app main component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss';
+import AddGoal from './components/AddGoal';
 import Navbar from './components/Navbar';
-
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <AddGoal />
     </div>
   );
 }
