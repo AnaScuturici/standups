f19c7c3e55dbb073e77ddf91fa50b260caa59b3d
ana scuturici <anascuturici@gmail.com>
2022-04-10 21:57:20 +0900

    update: goals component to use imported api call function

--- Frontend/src/components/Goals.jsx
+++ Frontend/src/components/Goals.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import axios from "axios";
 import { useState, useEffect } from 'react';
-
-const API_URL = "http://[::1]:3000/api/v1/standups";
-
-const getAPIData = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-}
+import { getAPIData } from "./goalsAPI";
 
 const Goals = ()  => {
     const [goals, setGoals] = useState([]);
