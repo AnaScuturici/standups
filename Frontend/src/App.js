2deb2d71e2aeceb0df9ffb175945e2415665320b
ana scuturici <anascuturici@gmail.com>
2022-04-10 21:07:32 +0900

    update: add navbar to app-main component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
-import Goals from './components/Goals';
-import axios from "axios";
-import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
 
-const API_URL = "http://[::1]:3000/api/v1/standups";
-
-const getAPIData = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-}
 
 function App() {
-  const [goals, setGoals] = useState([]);
-
-  useEffect(()=> {
-    let mounted = true;
-    getAPIData().then(items => {
-      if(mounted){
-        setGoals(items);
-      }
-    });
-    return () => mounted = false;
-  }, []);
-
   return (
     <div className="App">
-      <h1>Standups</h1>
-      <Goals goals={goals}/>
+      <Navbar/>
     </div>
   );
 }
