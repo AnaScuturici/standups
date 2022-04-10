29fb89a0a889a8d9d7f1e50e64d88a524e5892ad
ana scuturici <anascuturici@gmail.com>
2022-04-10 15:05:32 +0900

    add: pass data as props to child component

--- Frontend/src/components/Goals.jsx
+++ Frontend/src/components/Goals.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 function Goals(props) {
   return (
     <div>
-      <h3>Goals</h3>
+      <h3>API data</h3>
+      {props.goals.map(goal =>{
+          return (
+              <div key={goal.id}>
+                  <p>Goal: {goal.goal}</p>
+                  <p>Rating: {goal.rating}</p>
+              </div>
+          )
+      })}
     </div>
   )
 }
