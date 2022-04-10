a2e3a870b3b3bb81b07c8b95b3376ae06a656717
ana scuturici <anascuturici@gmail.com>
2022-04-10 21:06:14 +0900

    change: move api call to goals component

--- Frontend/src/components/Goals.jsx
+++ Frontend/src/components/Goals.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import axios from "axios";
+import { useState, useEffect } from 'react';
 
-function Goals(props) {
+const API_URL = "http://[::1]:3000/api/v1/standups";
+
+const getAPIData = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+}
+
+function Goals() {
+    const [goals, setGoals] = useState([]);
+
+  useEffect(()=> {
+    let mounted = true;
+    getAPIData().then(items => {
+      if(mounted){
+        setGoals(items);
+      }
+    });
+    return () => mounted = false;
+  }, []);
+
   return (
     <div>
       <h3>API data</h3>
-      {props.goals.map(goal =>{
+      {goals.map(goal =>{
           return (
               <div key={goal.id}>
                   <p>Goal: {goal.goal}</p>
+                  <p>Done: {goal.is_completed ? "Done" : "Not yet"}</p>
                   <p>Rating: {goal.rating}</p>
+                  <p>Added on: {goal.created_at}</p>
               </div>
           )
       })}
