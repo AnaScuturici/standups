48f19ce29d079da715d06da5b8331268d3a89d50
ana scuturici <anascuturici@gmail.com>
2022-04-12 15:38:35 +0900

    add: chart component in tracker - basic setup

--- Frontend/src/components/Tracker.jsx
+++ Frontend/src/components/Tracker.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import "./styles/Tracker.scss";
+import { Chart } from 'react-chartjs-2';
+import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
+
+ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
 
 const Tracker = () => {
   return (
     <div>
-      WEEKLY TRACKER
+      <h2>WEEKLY TRACKER</h2>
+      <div className='chart-div'>
+        <Chart
+          type="line"
+          data={{
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+              ],
+              borderWidth: 2
+          }],
+          options: {
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
+              }
+            }
+          }
+          }}
+        />
+      </div>
     </div>
   )
 }
