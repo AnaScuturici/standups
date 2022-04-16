fc7ead8cdcc67fe064401c160571bf621327b91c
ana scuturici <anascuturici@gmail.com>
2022-04-16 15:02:29 +0900

    add: render api data in chart

--- Frontend/src/components/Tracker.jsx
+++ Frontend/src/components/Tracker.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./styles/Tracker.scss";
+import { getAPIData } from "./goalsAPI";
 import { Chart } from 'react-chartjs-2';
 import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
 
 ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
 
 const Tracker = () => {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => { 
+   getAPIData().then(items => {
+        setGoals(items);
+    });
+  }, []);
+
+  const data = {
+    labels: goals.map(goal => goal.created_at),
+    datasets: [{
+      data: goals.map(goal => goal.rating),
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderWidth: 2
+    }]
+  }
+
   return (
     <div>
       <h2>WEEKLY TRACKER</h2>
       <div className='chart-div'>
         <Chart
           type="line"
-          data={{
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-              ],
-              borderWidth: 2
-          }],
-          options: {
-            maintainAspectRatio: false,
-            scales: {
-              y: {
-                beginAtZero: true,
-              }
-            }
-          }
-          }}
+          data={data}
         />
       </div>
     </div>
