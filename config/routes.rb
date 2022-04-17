93be56705c6a13d3878a805c93be62303970e87c
ana scuturici <anascuturici@gmail.com>
2022-04-17 23:10:31 +0900

    fix: root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "api/v1/standups#index"
+  root to: redirect("/api/v1/standups")
+  get "/api/v1/standups", to: "standups#index"
 end
